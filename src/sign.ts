811d37076751d33dfeeb499c3fe8b68e6a00e34b
Jacob Singer <jacobleesinger@gmail.com>
2020-06-15 17:02:06 -0700

    add sign function (#7)

---
+++ src/sign.ts
@@ -0,0 +1,12 @@
+import crypto from 'crypto';
+import stringify from 'fast-json-stable-stringify';
+import bs58 from 'bs58';
+
+export function sign (data: Record<string, unknown>, privateKey: string): string {
+  const stringifiedData = stringify(data);
+  const buf = Buffer.from(stringifiedData);
+  const hash = crypto.createHash('sha256');
+  hash.update(buf);
+  const signatureValueBuf = crypto.sign(null, hash.digest(), privateKey);
+  return bs58.encode(signatureValueBuf);
+}
