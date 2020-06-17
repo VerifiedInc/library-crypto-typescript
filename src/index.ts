c491841af80f94ef11456b6be29502f73b3b3841
jacobleesinger <jacobleesinger@gmail.com>
2020-06-17 10:34:45 -0700

    usability updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export { generateEccKeyPair } from './generateEccKeyPair';
+export { generateRsaKeyPair } from './generateRsaKeyPair';
 export { sign } from './sign';
 export { verify } from './verify';
+export { encrypt } from './encrypt';
+export { decrypt } from './decrypt';
