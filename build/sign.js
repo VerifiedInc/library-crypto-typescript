c9a9338fccbfd944abe74ac8640e25f430bff14b
jacobleesinger <jacobleesinger@gmail.com>
2020-06-24 10:30:00 -0700

    remove hash step from sign/verify

--- build/sign.js
+++ build/sign.js
@@ -10,9 +10,7 @@ var bs58_1 = __importDefault(require("bs58"));
 function sign(data, privateKey) {
     var stringifiedData = fast_json_stable_stringify_1.default(data);
     var buf = Buffer.from(stringifiedData);
-    var hash = crypto_1.default.createHash('sha256');
-    hash.update(buf);
-    var signatureValueBuf = crypto_1.default.sign(null, hash.digest(), privateKey);
+    var signatureValueBuf = crypto_1.default.sign(null, buf, privateKey);
     return bs58_1.default.encode(signatureValueBuf);
 }
 exports.sign = sign;
