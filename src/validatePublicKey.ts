62d91f8e9ae7c3bc67cf75c17af5df8d57e6a917
Ray Smets <rayjsmets@gmail.com>
2021-02-23 18:04:10 -0800

    [ValidatePublicKey] bug fix. Moved try catch.

--- src/validatePublicKey.ts
+++ src/validatePublicKey.ts
@@ -8,15 +8,15 @@ import { decodeKey } from './helpers';
  * @returns {boolean} true if valid key information
  */
 export function validatePublicKey (key: string, encoding: 'base58' | 'pem' = 'pem'): boolean {
-  // decode public key if necessary
-  const decodedKey = decodeKey(key, encoding);
-
-  // if we pass the key to crypto.verify as a buffer, it will assume pem format
-  // we need to convert it to a KeyObject first in order to use der formatted keys
-  const format = encoding === 'pem' ? 'pem' : 'der';
-  const type = encoding === 'pem' ? 'pkcs1' : 'spki';
-
   try {
+    // decode public key if necessary
+    const decodedKey = decodeKey(key, encoding);
+
+    // if we pass the key to crypto.verify as a buffer, it will assume pem format
+    // we need to convert it to a KeyObject first in order to use der formatted keys
+    const format = encoding === 'pem' ? 'pem' : 'der';
+    const type = encoding === 'pem' ? 'pkcs1' : 'spki';
+
     crypto.createPublicKey({ key: decodedKey, format, type });
   } catch (e) {
     throw new CryptoError(e.message, e.code);
