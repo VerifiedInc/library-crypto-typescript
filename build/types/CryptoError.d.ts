78e220e0d5a0c373d3b7de7da6fe3e9f16ae9c05
Ray Smets <rayjsmets@gmail.com>
2021-02-23 17:07:47 -0800

    [CryptError] code type.

--- build/types/CryptoError.d.ts
+++ build/types/CryptoError.d.ts
@@ -2,7 +2,6 @@
  * Class to encapsulate custom CryptoError.
  */
 export declare class CryptoError extends Error {
-    code: number | undefined;
     constructor(message: string, code?: number);
 }
 //# sourceMappingURL=CryptoError.d.ts.map
--- src/types/CryptoError.ts
+++ src/types/CryptoError.ts
@@ -2,14 +2,14 @@
  * Class to encapsulate custom CryptoError.
  */
 export class CryptoError extends Error {
-    code: number | undefined; // place holder if want to codify the errors
+  // code: number | undefined; // place holder if want to codify the errors
 
-    constructor (message: string, code?: number) {
-      super(message);
-      this.code = code;
+  constructor (message: string, code?: number) {
+    super(message);
+    // this.code = code;
 
-      // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
-      Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
-      this.name = CryptoError.name; // stack traces display correctly now
-    }
+    // see: typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
+    Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+    this.name = CryptoError.name; // stack traces display correctly now
+  }
 }
