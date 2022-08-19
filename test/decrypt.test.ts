fd446d6b22258e601d7a3bf6ac0c6fd601de0bb5
Ray Smets <rayjsmets@gmail.com>
2022-08-18 19:02:21 -0700

    [decrypt] tests

--- test/decrypt.test.ts
+++ test/decrypt.test.ts
@@ -1,22 +1,44 @@
 import crypto from 'crypto';
-import { EncryptedData, RSAPadding } from '@unumid/types';
+import { CredentialPb, CredentialStatus, EncryptedData, Proof, RSAPadding, UnsignedCredential, Credential, UnsignedCredentialPb, UnsignedString } from '@unumid/types';
 
-import { decrypt, decryptBytes } from '../src/decrypt';
-import { encrypt, encryptBytesHelper } from '../src/encrypt';
-import { sign } from '../src/sign';
+import { decryptBytes } from '../src/decrypt';
+import { encryptBytesHelper } from '../src/encrypt';
+import { signBytes } from '../src/sign';
 import { generateRsaKeyPair } from '../src/generateRsaKeyPair';
 import { generateEccKeyPair } from '../src/generateEccKeyPair';
 import { derToPem, decodeKey } from '../src/helpers';
 import { CryptoError } from '../src/types/CryptoError';
 
+/**
+ * Create cryptographic proof from byte array of a Protobuf object
+ * @param data
+ * @param privateKey
+ * @param method
+ * @param encoding
+ */
+export const createProof = (signature: string, method: string): Proof => {
+  const proof: Proof = {
+    created: new Date(),
+    signatureValue: signature,
+    type: 'secp256r1Signature2020',
+    verificationMethod: method,
+    proofPurpose: 'assertionMethod'
+  };
+
+  return (proof);
+};
+
 describe('decrypt', () => {
   let publicKey: string;
   let privateKey: string;
-  const data = { test: 'test' };
+  const data: UnsignedString = {
+    data: 'Hello World'
+  };
+  const dataBytes = UnsignedString.encode(data).finish();
   const subjectDid = 'did:unum:c92aed65-21c1-438f-b723-d2ee4a637a47#e939fbf0-7c81-49c9-b369-8ca502fcd19f';
   let encryptedData: EncryptedData;
   let encryptedCredential;
-  let credential;
+  let credential: Credential;
 
   beforeEach(() => {
     jest.spyOn(crypto, 'privateDecrypt');
@@ -31,45 +53,47 @@ describe('decrypt', () => {
       const keypair = await generateRsaKeyPair();
       privateKey = keypair.privateKey;
       publicKey = keypair.publicKey;
-      encryptedData = encrypt(subjectDid, publicKey, data);
+      encryptedData = encryptBytesHelper(subjectDid, publicKey, dataBytes);
 
       const eccKeyPair = await generateEccKeyPair();
 
-      const unsignedCredential = {
-        '@context': ['https://www.w3.org/2018/credentials/v1'],
+      const credentialSubject = {
+        id: 'did:unum:89460433-c0b7-4892-aeb2-f2ece77af141',
+        value: 'dummy value'
+      };
+
+      const credentialStatus: CredentialStatus = {
+        id: 'https://example.edu/status/24',
+        type: 'CredentialStatusList2017'
+      };
+
+      const unsignedCredential: UnsignedCredential = {
+        context: ['https://www.w3.org/2018/credentials/v1'],
         id: '0c93beb0-2605-4650-b698-3fd92eb110b9',
-        credentialSubject: {
-          id: 'did:unum:89460433-c0b7-4892-aeb2-f2ece77af141',
-          value: 'dummy value'
-        },
-        credentialStatus: {
-          uuid: 'c3974fa3-396e-42ee-81a9-9ab69efce031',
-          status: 'valid',
-          createdAt: '2020-05-26T23:07:12.770Z',
-          updatedAt: '2020-05-26T23:07:12.770Z'
-        },
+        credentialSubject: JSON.stringify(credentialSubject),
+        credentialStatus,
         issuer: 'did:unum:e1281297-268b-4700-8f17-7fa826effe35',
         type: ['VerifiableCredential', 'DummyCredential'],
-        issuanceDate: '2020-05-26T23:07:12.770Z'
+        issuanceDate: new Date('2020-05-26T23:07:12.770Z')
       };
       const issuerDid = 'did:unum:756450ab-ab01-420c-838e-cfa0bebdc2ba';
 
-      const signatureValue = sign(unsignedCredential, eccKeyPair.privateKey);
+      const unsignedCredentialBytes = UnsignedCredentialPb.encode(unsignedCredential).finish();
+      const signatureValue = signBytes(unsignedCredentialBytes, eccKeyPair.privateKey);
+
+      const proof = createProof(signatureValue, `${issuerDid}#5b134be0-7cb4-4983-95b1-bdec218cb55b`);
+
       credential = {
         ...unsignedCredential,
-        proof: {
-          created: '2020-05-26T23:07:12.770Z',
-          signatureValue,
-          proofPurpose: 'assertionMethod',
-          type: 'secp256r1Signature2020',
-          verificationMethod: `${issuerDid}#5b134be0-7cb4-4983-95b1-bdec218cb55b`
-        }
+        proof
       };
 
-      encryptedCredential = encrypt(
+      const credentialBytes = CredentialPb.encode(credential).finish();
+
+      encryptedCredential = encryptBytesHelper(
         credential.proof.verificationMethod,
         publicKey,
-        credential
+        credentialBytes
       );
     });
 
@@ -82,7 +106,7 @@ describe('decrypt', () => {
     });
 
     it('decrypts with the private key', () => {
-      decrypt(privateKey, encryptedData);
+      decryptBytes(privateKey, encryptedData);
 
       const privateKeyObj = {
         key: privateKey,
@@ -94,92 +118,14 @@ describe('decrypt', () => {
     });
 
     it('returns the decrypted data', () => {
-      const decryptedData = decrypt(privateKey, encryptedData);
+      const decryptedDataBytes = decryptBytes(privateKey, encryptedData);
+      const decryptedData = UnsignedString.decode(decryptedDataBytes);
       expect(decryptedData).toEqual(data);
     });
 
     it('decrypts an actual encrypted credential', () => {
-      const decryptedData = decrypt(privateKey, encryptedCredential);
-      expect(decryptedData).toEqual(credential);
-    });
-  });
-
-  describe('using pem encoding', () => {
-    const encoding = 'pem';
-    beforeAll(async () => {
-      const keypair = await generateRsaKeyPair(encoding);
-      privateKey = keypair.privateKey;
-      publicKey = keypair.publicKey;
-      encryptedData = encrypt(subjectDid, publicKey, data, encoding);
-
-      const eccKeyPair = await generateEccKeyPair(encoding);
-
-      const unsignedCredential = {
-        '@context': ['https://www.w3.org/2018/credentials/v1'],
-        id: '0c93beb0-2605-4650-b698-3fd92eb110b9',
-        credentialSubject: {
-          id: 'did:unum:89460433-c0b7-4892-aeb2-f2ece77af141',
-          value: 'dummy value'
-        },
-        credentialStatus: {
-          uuid: 'c3974fa3-396e-42ee-81a9-9ab69efce031',
-          status: 'valid',
-          createdAt: '2020-05-26T23:07:12.770Z',
-          updatedAt: '2020-05-26T23:07:12.770Z'
-        },
-        issuer: 'did:unum:e1281297-268b-4700-8f17-7fa826effe35',
-        type: ['VerifiableCredential', 'DummyCredential'],
-        issuanceDate: '2020-05-26T23:07:12.770Z'
-      };
-      const issuerDid = 'did:unum:756450ab-ab01-420c-838e-cfa0bebdc2ba';
-
-      const signatureValue = sign(unsignedCredential, eccKeyPair.privateKey, encoding);
-      credential = {
-        ...unsignedCredential,
-        proof: {
-          created: '2020-05-26T23:07:12.770Z',
-          signatureValue,
-          proofPurpose: 'assertionMethod',
-          type: 'secp256r1Signature2020',
-          verificationMethod: `${issuerDid}#5b134be0-7cb4-4983-95b1-bdec218cb55b`
-        }
-      };
-
-      encryptedCredential = encrypt(
-        credential.proof.verificationMethod,
-        publicKey,
-        credential,
-        encoding
-      );
-    });
-
-    beforeEach(() => {
-      jest.spyOn(crypto, 'privateDecrypt');
-    });
-
-    afterEach(() => {
-      jest.restoreAllMocks();
-    });
-
-    it('decrypts with the private key', () => {
-      decrypt(privateKey, encryptedData, encoding);
-
-      const privateKeyObj = {
-        key: privateKey,
-        padding: crypto.constants.RSA_PKCS1_PADDING
-      };
-
-      expect(crypto.privateDecrypt).toBeCalled();
-      expect((crypto.privateDecrypt as jest.Mock).mock.calls[0][0]).toEqual(privateKeyObj);
-    });
-
-    it('returns the decrypted data', () => {
-      const decryptedData = decrypt(privateKey, encryptedData, encoding);
-      expect(decryptedData).toEqual(data);
-    });
-
-    it('decrypts an actual encrypted credential', () => {
-      const decryptedData = decrypt(privateKey, encryptedCredential, encoding);
+      const decryptedDataBytes = decryptBytes(privateKey, encryptedCredential);
+      const decryptedData = CredentialPb.decode(decryptedDataBytes);
       expect(decryptedData).toEqual(credential);
     });
   });
@@ -187,49 +133,50 @@ describe('decrypt', () => {
   describe('using base58 encoding', () => {
     const encoding = 'base58';
     beforeAll(async () => {
-      const keypair = await generateRsaKeyPair(encoding);
+      const keypair = await generateRsaKeyPair();
       privateKey = keypair.privateKey;
       publicKey = keypair.publicKey;
-      encryptedData = encrypt(subjectDid, publicKey, data, encoding);
+      encryptedData = encryptBytesHelper(subjectDid, publicKey, dataBytes);
 
       const eccKeyPair = await generateEccKeyPair();
 
-      const unsignedCredential = {
-        '@context': ['https://www.w3.org/2018/credentials/v1'],
+      const credentialSubject = {
+        id: 'did:unum:89460433-c0b7-4892-aeb2-f2ece77af141',
+        value: 'dummy value'
+      };
+
+      const credentialStatus: CredentialStatus = {
+        id: 'https://example.edu/status/24',
+        type: 'CredentialStatusList2017'
+      };
+
+      const unsignedCredential: UnsignedCredential = {
+        context: ['https://www.w3.org/2018/credentials/v1'],
         id: '0c93beb0-2605-4650-b698-3fd92eb110b9',
-        credentialSubject: {
-          id: 'did:unum:89460433-c0b7-4892-aeb2-f2ece77af141',
-          value: 'dummy value'
-        },
-        credentialStatus: {
-          uuid: 'c3974fa3-396e-42ee-81a9-9ab69efce031',
-          status: 'valid',
-          createdAt: '2020-05-26T23:07:12.770Z',
-          updatedAt: '2020-05-26T23:07:12.770Z'
-        },
+        credentialSubject: JSON.stringify(credentialSubject),
+        credentialStatus,
         issuer: 'did:unum:e1281297-268b-4700-8f17-7fa826effe35',
         type: ['VerifiableCredential', 'DummyCredential'],
-        issuanceDate: '2020-05-26T23:07:12.770Z'
+        issuanceDate: new Date('2020-05-26T23:07:12.770Z')
       };
       const issuerDid = 'did:unum:756450ab-ab01-420c-838e-cfa0bebdc2ba';
 
-      const signatureValue = sign(unsignedCredential, eccKeyPair.privateKey);
+      const unsignedCredentialBytes = UnsignedCredentialPb.encode(unsignedCredential).finish();
+      const signatureValue = signBytes(unsignedCredentialBytes, eccKeyPair.privateKey);
+
+      const proof = createProof(signatureValue, `${issuerDid}#5b134be0-7cb4-4983-95b1-bdec218cb55b`);
+
       credential = {
         ...unsignedCredential,
-        proof: {
-          created: '2020-05-26T23:07:12.770Z',
-          signatureValue,
-          proofPurpose: 'assertionMethod',
-          type: 'secp256r1Signature2020',
-          verificationMethod: `${issuerDid}#5b134be0-7cb4-4983-95b1-bdec218cb55b`
-        }
+        proof
       };
 
-      encryptedCredential = encrypt(
+      const credentialBytes = CredentialPb.encode(credential).finish();
+
+      encryptedCredential = encryptBytesHelper(
         credential.proof.verificationMethod,
         publicKey,
-        credential,
-        encoding
+        credentialBytes
       );
     });
 
@@ -242,12 +189,10 @@ describe('decrypt', () => {
     });
 
     it('decrypts with the private key', () => {
-      decrypt(privateKey, encryptedData, encoding);
-      const decodedKey = decodeKey(privateKey, encoding);
-      const privateKeyPem = derToPem(decodedKey, 'private');
+      decryptBytes(privateKey, encryptedData);
 
       const privateKeyObj = {
-        key: privateKeyPem,
+        key: privateKey,
         padding: crypto.constants.RSA_PKCS1_PADDING
       };
 
@@ -256,12 +201,14 @@ describe('decrypt', () => {
     });
 
     it('returns the decrypted data', () => {
-      const decryptedData = decrypt(privateKey, encryptedData, encoding);
+      const decryptedDataBytes = decryptBytes(privateKey, encryptedData);
+      const decryptedData = UnsignedString.decode(decryptedDataBytes);
       expect(decryptedData).toEqual(data);
     });
 
     it('decrypts an actual encrypted credential', () => {
-      const decryptedData = decrypt(privateKey, encryptedCredential, encoding);
+      const decryptedDataBytes = decryptBytes(privateKey, encryptedCredential);
+      const decryptedData = CredentialPb.decode(decryptedDataBytes);
       expect(decryptedData).toEqual(credential);
     });
   });
